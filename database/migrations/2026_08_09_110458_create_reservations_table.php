148a4f49ae90c1e5f4744a6c5292861ae67d9f0f
adsf adsf <josegoviaramirez@gmail.com>
2026-09-24 12:01:54 +0200

    Mejoras en las migraciones

--- database/migrations/2026_08_09_110458_create_reservations_table.php
+++ database/migrations/2026_08_09_110458_create_reservations_table.php
@@ -37,8 +37,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('reservations', function (Blueprint $table) {
-            Schema::dropIfExists('reservations');
-        });
+        Schema::dropIfExists('reservations');
     }
 };
