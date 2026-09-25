7d31aff9079e34b328242614af367b4bead41949
adsf adsf <josegoviaramirez@gmail.com>
2026-09-25 10:20:37 +0200

    Modificado Dockerfile.render para Render

--- vite.config.js
+++ vite.config.js
@@ -5,23 +5,33 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
+                // CSS comunes
+                'resources/css/app.css',
                 'resources/css/body.css',
                 'resources/css/header.css',
                 'resources/css/footer.css',
-                'resources/css/public.css',
-                'resources/css/calendar.css',
+
+                // CSS de usuarios
+                'resources/css/navbar.css',
                 'resources/css/subtitle.css',
-                'resources/css/form.css',
+                'resources/css/welcome.css',
                 'resources/css/responsive.css',
 
+                // CSS específicos
+                'resources/css/public.css',
+                'resources/css/calendar.css',
+                'resources/css/form.css',
+                'resources/css/dashboard.css',
+                'resources/css/table.css',
+
+                // JavaScript
                 'resources/js/app.js',
                 'resources/js/layout.js',
                 'resources/js/countdown-timer.js',
                 'resources/js/court-reservation-calendar.js',
                 'resources/js/calendar-manager.js',
                 'resources/js/public-courts-calendar.js',
-                'resources/js/validation.js'
+                'resources/js/validation.js',
             ],
             refresh: true,
         }),
